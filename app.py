6ca8e7b88c266e761472dc765910f6dd8308ecbf
1538621225 <1538621225@qq.com>
2018-01-07 21:32:09 +0800

    add app.py

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask
+from flask import render_template,redirect,url_for
 
 app = Flask(__name__)
 app.config.update
 ({
     'SECRET_KEY':'a random string'
 })
-app.config.from_pyfile('path/to/config.py')
+#app.config.from_pyfile('path/to/config.py')
+
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return redirect(url_for('user_index',username='default'))
+
+@app.route('/user/<username>')
+def user_index(username):
+    return render_template('user_index.html',username=username)
 
 if __name__ == '__main__':
     app.run()
